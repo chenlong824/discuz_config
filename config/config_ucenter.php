1317432568e948753553ab31948a5b7e3186e85d
chenlong89824 <chenlong89824@163.com>
2020-08-10 15:47:39 +0800

    fix error

---
+++ config/config_ucenter.php
@@ -0,0 +1,20 @@
+<?php
+
+
+define('UC_CONNECT', 'mysql');
+
+define('UC_DBHOST', '127.0.0.1');
+define('UC_DBUSER', 'root');
+define('UC_DBPW', '123456');
+define('UC_DBNAME', 'ultrax');
+define('UC_DBCHARSET', 'utf8');
+define('UC_DBTABLEPRE', '`ultrax`.pre_ucenter_');
+define('UC_DBCONNECT', 0);
+
+define('UC_CHARSET', 'utf-8');
+define('UC_KEY', 'b5TdI084mdT5B8acwbkdV8D9n9sa84NfI4W82cO390A07eP2j5T9J1p495Fey404');
+define('UC_API', 'http://192.168.180.92/uc_server');
+define('UC_APPID', '1');
+define('UC_IP', '');
+define('UC_PPP', 20);
+?>
